02ce809b907705450cc5d692c3aa89bea072e60c
Мария Мелентьева <97579059+Wersu@users.noreply.github.com>
2022-03-11 23:54:05 +0500

    Добавили свой вариант планирования процессов (рандом)

--- Лаба1/Program.cs
+++ Лаба1/Program.cs
@@ -6,63 +6,64 @@ namespace Лаба1
 {
     public class Program
     {
-        private static string savePath = Environment.ProcessPath.Substring(0, Environment.ProcessPath.LastIndexOf('\\')) + "\\Processes.txt";
+        private static string savePath = Environment.ProcessPath.Substring(0, Environment.ProcessPath.LastIndexOf('\\')) + "\\Processes.txt"; //Путь до файла с сохранением
 
         static void Main(string[] args)
         {
-            MakeFCFSProcess(GetTasksFromFile());
-            MakeRRProcess(GetTasksFromFile());
-            MakeSJFProcess(GetTasksFromFile());
+            MakeFCFSProcess(GetTasksFromFile());//Запускаем процессор FCFS
+            MakeRRProcess(GetTasksFromFile());//Запускаем процессор RR
+            MakeSJFProcess(GetTasksFromFile());//Запускаем процеесор SJF
+            MakeRProcess(GetTasksFromFile());//Запускаем процессор свой вариант
 
             Console.ReadKey();
         }
 
         private static List<SystemTask> GetTasksFromFile()
         {
-            List<SystemTask> result = new List<SystemTask>();
+            List<SystemTask> result = new List<SystemTask>();//Результат
 
-            if (!File.Exists(savePath))
-                LoadDefaultSaveFile();
+            if (!File.Exists(savePath))//Проверяем создан ли файл
+                LoadDefaultSaveFile();//Если нет, то создаём по умолчанию
 
-            foreach (var item in File.ReadAllLines(savePath))
-                result.Add(SystemTask.Parse(item));
+            foreach (var item in File.ReadAllLines(savePath))//Перечисляем каждую строку файла
+                result.Add(SystemTask.Parse(item));//Превращаем строку в SystemTask
 
-            if (result.Count==0)
+            if (result.Count==0)//Если в итоге не создались задачи, то загружаем по умолчанию
             {
-                LoadDefaultSaveFile();
-                return GetTasksFromFile();
+                LoadDefaultSaveFile();//Загружаем файл по умолчанию
+                return GetTasksFromFile();//Рекурсия
             }
 
-            return result;
+            return result;//Вовзращаем результат
         }
 
-        private static void LoadDefaultSaveFile()
+        private static void LoadDefaultSaveFile()//Загрузка файла по умолчанию
         {
-            if (File.Exists(savePath))
+            if (File.Exists(savePath))//Если файл есть, то удаляем
                 File.Delete(savePath);
 
-            File.Create(savePath).Close();
+            File.Create(savePath).Close();//Создаём файл и закрываем поток (чтобы можно было редактировать дальше)
 
-            List<string> lines = new List<string>() {
+            List<string> lines = new List<string>() {//Список строк, которые будут записаны в файл
             "p0|6|0|0",
             "p1|2|2|0",
             "p2|7|6|0",
             "p3|5|0|0",
             };
 
-            File.WriteAllLines(savePath, lines);
+            File.WriteAllLines(savePath, lines);//Записываем строки в файл
         }
 
         private static void MakeFCFSProcess(List<SystemTask> tasks)
         {
-            FCFSProcessor processor = new FCFSProcessor(tasks);
+            FCFSProcessor processor = new FCFSProcessor(tasks);//Создаём процессор
 
-            processor.StartProcess();
+            processor.StartProcess();//Запускаем
 
             Console.WriteLine("FCFS:");
-            foreach (var item in tasks)
+            foreach (var item in tasks)//Выводим все обработанные задачи
                 Console.WriteLine(item.ToString());
-            Console.WriteLine($"Эффектикность: {processor.Efficiency}");
+            Console.WriteLine($"Эффектикность: {processor.Efficiency}");//Выводим эффективность
         }
 
         private static void MakeRRProcess(List<SystemTask> tasks)
@@ -84,6 +85,18 @@ namespace Лаба1
             processor.StartProcess();
 
             Console.WriteLine("\n\nSJF:");
+            foreach (var item in tasks)
+                Console.WriteLine(item.ToString());
+            Console.WriteLine($"Эффектикность: {processor.Efficiency}");
+        }
+
+        private static void MakeRProcess(List<SystemTask> tasks)
+        {
+            RProcessor processor = new RProcessor(tasks);
+
+            processor.StartProcess();
+
+            Console.WriteLine("\n\nСвой вариант:");
             foreach (var item in tasks)
                 Console.WriteLine(item.ToString());
             Console.WriteLine($"Эффектикность: {processor.Efficiency}");
@@ -348,6 +361,74 @@ namespace Лаба1
                 {
                     lastStep=0;
                     lastIndex++;
+                }
+
+                UpdateCurrentExecutionTasks();//Обновляем текущие задачи (удаляем ненужные + сортируем)
+            }
+        }
+
+        private void UpdateCurrentExecutionTasks()//Обновление и сортировка текущих задач
+        {
+            foreach (var item in allTasks)//Перечиляем все задачи
+            {
+                if (currentExecutionTasks.Contains(item))//Если эта задача есть в текущих, то пропускаем
+                    continue;
+
+                if (item.IsValid())//Если задача готова к началу работы, то добавляем в текущие
+                    currentExecutionTasks.Add(item);
+                else if (item.TimeDelay>0)//Если не готова, то уменьшаем ей задержку до выполнения
+                    item.TimeDecrease();
+            }
+
+            List<SystemTask> toRemove = new List<SystemTask>();//Задачи к удалению из текущих
+
+            foreach (var item in currentExecutionTasks)//Перечисляем текущие задачи
+                if (!item.IsValid())//Если задача уже не акатуальна, то добавляем на удаление
+                    toRemove.Add(item);
+
+            foreach (var item in toRemove)//Перечисляем все задачи для удаления
+                currentExecutionTasks.Remove(item);//Удаляем задачу
+        }
+    }
+
+    public class RProcessor
+    {
+        private List<SystemTask> allTasks = new List<SystemTask>();//Все задачи
+
+        private List<SystemTask> currentExecutionTasks = new List<SystemTask>();//Текущие задачи
+
+        private int totalWaiters = 0;
+        private int totalOperations = 0;
+
+        public float Efficiency { get { return (float)totalWaiters/totalOperations; } }
+
+
+        public RProcessor(List<SystemTask> AllTasks)
+        {
+            allTasks = new List<SystemTask>(AllTasks);
+        }
+
+        public void StartProcess()//Начать выполнение процесса
+        {
+            Random rnd = new Random();
+
+            UpdateCurrentExecutionTasks();//Обновляем текущие задачи (удаляем ненужные + сортируем)
+
+            while (currentExecutionTasks.Count != 0)//Пока есть текущие задачи
+            {
+                int currentIndex = rnd.Next(0, currentExecutionTasks.Count);
+
+                for (int i = 0; i<currentExecutionTasks.Count; i++)//Перечисляем отсортированные текущие задачи
+                {
+                    if (i==currentIndex)
+                        currentExecutionTasks[i].Execute();//Выполняем первую текущую задачу
+                    else
+                    {
+                        currentExecutionTasks[i].Wait();//Остальные задачи ожидают
+                        totalWaiters++;
+                    }
+
+                    totalOperations++;
                 }
 
                 UpdateCurrentExecutionTasks();//Обновляем текущие задачи (удаляем ненужные + сортируем)
